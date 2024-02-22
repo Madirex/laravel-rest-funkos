031bb72479239366f891ce51693ab3067e38319a
Madirex <angel@madirex.com>
2024-02-22 09:44:57 +0100

    translated to ES

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -10,15 +10,15 @@
                     <div class="card-body">
                         @if (session('resent'))
                             <div class="alert alert-success" role="alert">
-                                {{ __('A fresh verification link has been sent to your email address.') }}
+                                {{ __('Ha sido enviado un email de notificación a tu cuenta.') }}
                             </div>
                         @endif
 
-                        {{ __('Before proceeding, please check your email for a verification link.') }}
-                        {{ __('If you did not receive the email') }},
+                        {{ __('Antes de continuar, por favor, revisa tu correo electrónico y accede al enlace de verificación.') }}
+                        {{ __('Si no has recibido el email') }},
                         <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                             @csrf
-                            <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                            <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('Haz clic aquí para pedir otrog') }}</button>.
                         </form>
                     </div>
                 </div>
