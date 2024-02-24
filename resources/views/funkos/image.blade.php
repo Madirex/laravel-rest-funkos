428ca98a13250232cbb421b4dc639d5c46c62522
Madirex <angel@madirex.com>
2024-02-24 13:44:34 +0100

    better styles

--- resources/views/funkos/image.blade.php
+++ resources/views/funkos/image.blade.php
@@ -3,46 +3,54 @@
 @section('title', 'Editar imagen de Funko')
 
 @section('content')
-    <h1>Editar imagen de Funko</h1>
+    <h1><i class="bi bi-pencil-square"></i> Editar imagen de Funko</h1>
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title"><i class="fas fa-tag"></i> {{ $funko->name }}</h5>
+            <p class="card-text"><i class="fas fa-image"></i> Imagen actual:
+                @if($funko->image != Funko::$IMAGE_DEFAULT)
+                    <img alt="Imagen del Funko" class="img-fluid funko-image" src="{{ asset('storage/' . $funko->image) }}">
+                @else
+                    <img alt="Imagen por defecto" class="img-fluid funko-image" src="{{ Funko::$IMAGE_DEFAULT }}">
+                @endif
+            </p>
+            <form id="updateImageForm" action="{{ route("funkos.updateImage", $funko->id) }}" method="post" enctype="multipart/form-data">
+                @csrf
+                @method('PATCH')
+                <div class="form-group">
+                    <label for="image"><i class="bi bi-card-image"></i> Nueva imagen:</label>
+                    <input accept="image/*" class="form-control-file" id="image" name="image" required type="file" aria-describedby="imageHelp">
+                    <small id="imageHelp" class="form-text text-muted">Por favor, sube una imagen para el Funko.</small>
+                </div>
 
-    @if ($errors->any())
-        <div class="alert alert-danger alert-dismissible">
-            <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#confirmUpdateModal"><i class="bi bi-check-circle-fill"></i> Actualizar</button>
+            </form>
         </div>
-        <br/>
-    @endif
+    </div>
 
-    <dl class="row">
-        <dt class="col-sm-2">ID:</dt>
-        <dd class="col-sm-10">{{$funko->id}}</dd>
-        <dt class="col-sm-2">Nombre:</dt>
-        <dd class="col-sm-10">{{$funko->name}}</dd>
-        <dt class="col-sm-2">Imagen:</dt>
-        <dd class="col-sm-10">
-            @if($funko->image != Funko::$IMAGE_DEFAULT)
-                <img alt="Imagen del Funko" class="img-fluid" src="{{ asset('storage/' . $funko->image) }}">
-            @else
-                <img alt="Imagen por defecto" class="img-fluid" src="{{ Funko::$IMAGE_DEFAULT }}">
-            @endif
-        </dd>
-    </dl>
+    <br/>
 
-    <form action="{{ route("funkos.updateImage", $funko->id) }}" method="post" enctype="multipart/form-data">
-        @csrf
-        @method('PATCH')
-        <div class="form-group">
-            <label for="image">Imagen:</label>
-            <input accept="image/*" class="form-control-file" id="image" name="image" required type="file">
-            <small class="text-danger"></small>
+    <a class="btn btn-primary" href="{{ route('funkos.index') }}"><i class="fas fa-arrow-left"></i> Volver</a>
+
+    <!-- Modal de Confirmación de actualización -->
+    <div class="modal fade" id="confirmUpdateModal" tabindex="-1" role="dialog" aria-labelledby="confirmUpdateModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="confirmUpdateModalLabel">Confirmar Actualización</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    ¿Estás seguro de que quieres actualizar la imagen?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                    <button type="button" class="btn btn-primary" onclick="document.getElementById('updateImageForm').submit();">Actualizar</button>
+                </div>
+            </div>
         </div>
-
-        <button class="btn btn-primary" type="submit">Actualizar</button>
-        <a class="btn btn-secondary mx-2" href="{{ route('funkos.index') }}">Volver</a>
-    </form>
+    </div>
 
 @endsection
